417d265d927f435a6c48a1ace7c5e0a32ec465f9
Eecodev <mnasser.bbk@gmail.com>
2023-12-22 12:55:50 +0100

    seeder added and db populated

--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Book;
 
 class BookTableSeeder extends Seeder
 {
@@ -14,6 +15,31 @@ class BookTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $books = file_get_contents(__DIR__ . '/books_db.json');
+        $books = json_decode($books, true);
+        foreach ($books as $book){
+            $newBook = new Book();
+
+            $newBook->title = $book['title'];
+            $newBook->image = $book['thumbnailUrl'];
+            $newBook->save();
+        }
+        //dd($books);
+        // $recipes = config('db.recipes');
+        // dd($recipes);
+        // $houses = array_map('str_getcsv', file(__DIR__ .'/houses.csv'));
+        // dd($houses);
+
+        // foreach ($houses as $key => $house){
+        //     if($key > 0){
+        //         $newHouse = new House();
+        //         $newHouse->image = $house[0];
+
+
+        //         $newHouse->save();
+        //     }
+
+        // }
+
     }
 }
